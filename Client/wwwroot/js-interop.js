650f6afd763a046bd7e6661735365b2e5832217a
jajuiscool <jacobjuarez1996@gmail.com>
2024-09-28 21:00:38 -0500

    Mobile Scroll Fix

--- Client/wwwroot/js-interop.js
+++ Client/wwwroot/js-interop.js
@@ -195,7 +195,7 @@ window.addEventListener('scroll', () => {
         body.classList.remove('scroll-up');
     }
 
-    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+    if (currentScroll > lastScroll && !body.classList.contains('scroll-down') && currentScroll > 50) {
         body.classList.remove('scroll-up');
         body.classList.add('scroll-down');
     }
